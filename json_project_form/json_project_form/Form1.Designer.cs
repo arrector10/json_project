1d21c9c9806f45ea13995306dab15c5f25eb230d
arrector10 <alicia.r.rector@gmail.com>
2019-07-09 20:53:57 -0400

    file stream

--- json_project_form/json_project_form/Form1.Designer.cs
+++ json_project_form/json_project_form/Form1.Designer.cs
@@ -40,10 +40,11 @@
             // 
             // txt_output
             // 
-            this.txt_output.Location = new System.Drawing.Point(44, 28);
+            this.txt_output.Location = new System.Drawing.Point(48, 21);
             this.txt_output.Multiline = true;
             this.txt_output.Name = "txt_output";
-            this.txt_output.Size = new System.Drawing.Size(568, 99);
+            this.txt_output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_output.Size = new System.Drawing.Size(964, 222);
             this.txt_output.TabIndex = 0;
             // 
             // groupBox1
@@ -51,31 +52,32 @@
             this.groupBox1.Controls.Add(this.txt_output);
             this.groupBox1.Location = new System.Drawing.Point(31, 28);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(650, 133);
+            this.groupBox1.Size = new System.Drawing.Size(1044, 260);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "JSON file output";
             // 
             // btn_json_file
             // 
-            this.btn_json_file.Location = new System.Drawing.Point(117, 179);
+            this.btn_json_file.Location = new System.Drawing.Point(117, 294);
             this.btn_json_file.Name = "btn_json_file";
             this.btn_json_file.Size = new System.Drawing.Size(75, 23);
             this.btn_json_file.TabIndex = 2;
             this.btn_json_file.Text = "Display";
             this.btn_json_file.UseVisualStyleBackColor = true;
+            this.btn_json_file.Click += new System.EventHandler(this.btn_json_file_Click);
             // 
             // txt_enter
             // 
-            this.txt_enter.Location = new System.Drawing.Point(75, 226);
+            this.txt_enter.Location = new System.Drawing.Point(117, 377);
             this.txt_enter.Multiline = true;
             this.txt_enter.Name = "txt_enter";
-            this.txt_enter.Size = new System.Drawing.Size(568, 115);
+            this.txt_enter.Size = new System.Drawing.Size(901, 115);
             this.txt_enter.TabIndex = 3;
             // 
             // btn_artist
             // 
-            this.btn_artist.Location = new System.Drawing.Point(117, 363);
+            this.btn_artist.Location = new System.Drawing.Point(266, 534);
             this.btn_artist.Name = "btn_artist";
             this.btn_artist.Size = new System.Drawing.Size(96, 23);
             this.btn_artist.TabIndex = 4;
@@ -84,7 +86,7 @@
             // 
             // btn_works
             // 
-            this.btn_works.Location = new System.Drawing.Point(324, 363);
+            this.btn_works.Location = new System.Drawing.Point(535, 534);
             this.btn_works.Name = "btn_works";
             this.btn_works.Size = new System.Drawing.Size(91, 23);
             this.btn_works.TabIndex = 5;
@@ -93,7 +95,7 @@
             // 
             // btn_clear
             // 
-            this.btn_clear.Location = new System.Drawing.Point(535, 363);
+            this.btn_clear.Location = new System.Drawing.Point(801, 534);
             this.btn_clear.Name = "btn_clear";
             this.btn_clear.Size = new System.Drawing.Size(75, 23);
             this.btn_clear.TabIndex = 6;
@@ -105,7 +107,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(723, 412);
+            this.ClientSize = new System.Drawing.Size(1106, 659);
             this.Controls.Add(this.btn_clear);
             this.Controls.Add(this.btn_works);
             this.Controls.Add(this.btn_artist);
--- json_project_form/json_project_form/Form1.cs
+++ json_project_form/json_project_form/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace json_project_form
 {
     public partial class Form1 : Form
     {
+        private readonly string _path = @"C:\Users\alici\source\repos\json_project\artists.json";
+
         public Form1()
         {
             InitializeComponent();
@@ -28,5 +32,50 @@ namespace json_project_form
             txt_enter.Clear();
 
         }
+
+        private void btn_json_file_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string jsonFromFile;
+                using (var sreader = new StreamReader(_path))
+                {
+                    jsonFromFile = sreader.ReadToEnd();
+                }
+
+                txt_output.Text = jsonFromFile;
+
+                //var artistFromJson = JsonConvert.DeserializeObject<Artist>(jsonFromFile);
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
     }
+
+
+    public class Rootobject
+    {
+        public Artist[] artists { get; set; }
+    }
+
+    public class Artist
+    {
+        public string name { get; set; }
+        public string DOB { get; set; }
+        public string DOD { get; set; }
+        public string married { get; set; }
+        public Work[] works { get; set; }
+    }
+
+    public class Work
+    {
+        public string title { get; set; }
+        public string type { get; set; }
+        public string year { get; set; }
+    }
+
 }
